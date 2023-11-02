beb9911d0d1c74ca1db7239518eadb579298ab4a
Ankan Pal <itsyourap@gmail.com>
2023-11-02 16:10:24 +0530

    Implement limitation of guesses

--- Task1_GuessTheNumber/src/GuessTheNumber.java
+++ Task1_GuessTheNumber/src/GuessTheNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class GuessTheNumber {
     private static final int MIN_RANDOM_NUMBER = 1;
     private static final int MAX_RANDOM_NUMBER = 100;
+    private static final int MAX_GUESSES = 10;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,13 +11,13 @@ public class GuessTheNumber {
         int randomNumber = (int) (Math.random() * (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1) + MIN_RANDOM_NUMBER);
         System.out.printf("A random number has been generated between %d and %d\n", MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
         System.out.println("Try too guess it!");
-        System.out.println("You have unlimited tries");
+        System.out.printf("You have %d tries\n", MAX_GUESSES);
 
-        int guess = -1;
+        int guessNumber = 1;
 
-        while (guess != randomNumber) {
+        while (guessNumber <= MAX_GUESSES) {
             System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
+            int guess = scanner.nextInt();
 
             if (guess < randomNumber) {
                 System.out.println("Your guess is too low!");
@@ -24,7 +25,14 @@ public class GuessTheNumber {
                 System.out.println("Your guess is too high!");
             } else {
                 System.out.println("Congrats! You guessed the number correctly!");
+                break;
             }
+
+            guessNumber++;
+        }
+
+        if (guessNumber > MAX_GUESSES) {
+            System.out.println("You ran out of guesses!");
         }
 
         System.out.println("Thank you for playing!");

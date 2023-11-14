472c4db13a0246df6d2568923e15d00bc9cd36a4
Ankan Pal <itsyourap@gmail.com>
2023-11-14 21:55:42 +0530

    Add Javadocs to DatabaseManager class

--- Task2_ATM/src/database/DatabaseManager.java
+++ Task2_ATM/src/database/DatabaseManager.java
@@ -10,12 +10,38 @@ import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * DatabaseManager is a singleton class that manages the database connection and provides methods to interact with the
+ * database.
+ * <p>
+ * The database is initialized using the initializeDatabase.sql file in the same package.
+ * The database is created if it doesn't exist.
+ * The database is created with 3 tables:
+ *  <ul>
+ *      <li>accounts</li>
+ *      <li>transaction_type</li>
+ *      <li>transactions</li>
+ * </ul>
+ *
+ * @author <a href="https://github.com/itsyourap">Ankan Pal</a>
+ */
 public class DatabaseManager {
+    // Singleton instance
     private static DatabaseManager instance;
 
+    // Database URL
     private static final String DB_URL = "jdbc:sqlite:Task2_ATM/atm_database.db";
+
+    // Database initialization file
+    private static final String DATABASE_INITIALIZATION_FILE = "initializeDatabase.sql";
+
+    // Database connection
     private Connection conn;
 
+    /**
+     * Returns the singleton instance of DatabaseManager
+     * @return DatabaseManager instance
+     */
     public static DatabaseManager getInstance() {
         synchronized (DatabaseManager.class) {
             if (instance == null)
@@ -24,11 +50,21 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Private constructor to prevent instantiation
+     * Initializes the database connection and creates the database if it doesn't exist
+     */
     private DatabaseManager() {
         initializeDatabaseConnection();
         createDatabaseIfNotExists();
     }
 
+    /**
+     * Initializes the database connection
+     * Prints an error message if the connection fails
+     * Called by the constructor
+     * @see DatabaseManager#DatabaseManager()
+     */
     private void initializeDatabaseConnection() {
         try {
             conn = DriverManager.getConnection(DB_URL);
@@ -37,12 +73,24 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Creates the database if it does not already exist
+     * Checks if tables 'accounts', 'transaction_type', and 'transactions' exist
+     * If any of the tables do not exist, calls the initializeDatabase method
+     * @see DatabaseManager#initializeDatabase()
+     */
     private void createDatabaseIfNotExists() {
         if (!doesTableExist("accounts") || !doesTableExist("transaction_type") || !doesTableExist("transactions")) {
             initializeDatabase();
         }
     }
 
+    /**
+     * Checks if a table exists in the database
+     *
+     * @param tableName the name of the table to check
+     * @return true if the table exists, false otherwise
+     */
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     private boolean doesTableExist(String tableName) {
         boolean tableExists = false;
@@ -59,9 +107,14 @@ public class DatabaseManager {
         return tableExists;
     }
 
+    /**
+     * Initializes the database by executing SQL queries from the {@link #DATABASE_INITIALIZATION_FILE}  file
+     * Prints an error message if the execution fails
+     * @see DatabaseManager#DATABASE_INITIALIZATION_FILE
+     */
     private void initializeDatabase() {
         try (Statement statement = conn.createStatement()) {
-            InputStream inputStream = getClass().getResourceAsStream("initializeDatabase.sql");
+            InputStream inputStream = getClass().getResourceAsStream(DATABASE_INITIALIZATION_FILE);
             assert inputStream != null;
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
@@ -87,6 +140,14 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Retrieves an account based on the provided user ID and user PIN.
+     *
+     * @param userId  the user ID of the account
+     * @param userPin the user PIN of the account
+     * @return the {@link AccountModel} object representing the account, or null if the account is not found or an error occurs
+     * @see AccountModel
+     */
     public AccountModel getAccount(String userId, int userPin) {
         String query = "SELECT id, user_id, user_name, balance FROM accounts WHERE user_id = ? AND user_pin = ?";
         AccountModel account = null;
@@ -110,6 +171,13 @@ public class DatabaseManager {
         return account;
     }
 
+    /**
+     * Retrieves an account based on the provided user ID.
+     *
+     * @param userId  the user ID of the account
+     * @return the {@link AccountModel} object representing the account, or null if the account is not found or an error occurs
+     * @see AccountModel
+     */
     public AccountModel getAccount(String userId) {
         String query = "SELECT id, user_id, user_name, balance FROM accounts WHERE user_id = ?";
         AccountModel account = null;
@@ -132,6 +200,14 @@ public class DatabaseManager {
         return account;
     }
 
+    /**
+     * Retrieves an account based on the provided account ID.
+     *
+     * @param accountId  the account ID of the account
+     * @return the {@link AccountModel} object representing the account,
+     * or null if the account is not found or an error occurs
+     * @see AccountModel
+     */
     public AccountModel getAccount(long accountId) {
         String query = "SELECT id, user_id, user_name, balance FROM accounts WHERE id = ?";
         AccountModel account = null;
@@ -154,6 +230,12 @@ public class DatabaseManager {
         return account;
     }
 
+    /**
+     * Adds a transaction to the database and updates the account balance.
+     *
+     * @param transaction  the transaction to be added
+     * @return true if the transaction is successfully added, false otherwise
+     */
     public boolean addTransaction(TransactionModel transaction) {
         String operation;
         if (
@@ -196,7 +278,7 @@ public class DatabaseManager {
             balanceUpdateStatement.executeUpdate();
             conn.commit();
 
-            if (transaction.getTransactionType() == TransactionModel.TransactionType.TRANSFER_WITHDRAW){
+            if (transaction.getTransactionType() == TransactionModel.TransactionType.TRANSFER_WITHDRAW) {
                 TransactionModel counterTransaction = new TransactionModel();
                 counterTransaction.setAccountId(transaction.getOtherPartyAccountId());
                 counterTransaction.setAmount(transaction.getAmount());
@@ -213,6 +295,12 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Retrieves the transaction history for a given account.
+     *
+     * @param account the account for which to retrieve the transaction history
+     * @return an ArrayList of TransactionModel objects representing the transaction history of the account
+     */
     public ArrayList<TransactionModel> getTransactionHistory(AccountModel account) {
         ArrayList<TransactionModel> transactionList = new ArrayList<>();
 
